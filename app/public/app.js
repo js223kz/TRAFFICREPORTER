863e61bfb7b7bb690f9de98dd0470c9cc3e01799
js223kz <js223kz@student.lnu.se>
2016-03-04 18:44:18 +0100

    added a maincontroller

--- app/public/app.js
+++ app/public/app.js
@@ -1,1 +1,13 @@
-let trafficApp = angular.module('trafficApp', []);
+let trafficApp = angular.module('trafficApp', ['ngRoute']);
+
+trafficApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'mainView.html',
+        controller: 'MainController'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
