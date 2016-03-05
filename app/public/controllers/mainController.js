e76b6471e3048dfed82efc5a2fe8a7aba8e27ad3
js223kz <js223kz@student.lnu.se>
2016-03-05 14:00:11 +0100

    added map to view

--- app/public/controllers/mainController.js
+++ app/public/controllers/mainController.js
@@ -1,6 +1,12 @@
 "use strict";
 trafficApp.controller('MainController', function($scope, TrafficInfoService){
+    
+    $scope.map = L.map('map').setView([62, 17], 5);
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo($scope.map);
 
+    
     TrafficInfoService.get()
             .then(function(data) {
                 $scope.data = data;
